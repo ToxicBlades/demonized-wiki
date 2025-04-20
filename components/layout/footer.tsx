11d0659d8341830205e74cbdc9b7a39ff3553853
Nick <128822732+ToxicBlades@users.noreply.github.com>
2025-04-21 02:07:13 +0300

    fix: linter

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,52 +1,66 @@
-"use client"
-import Link from "next/link"
-import { Github } from 'lucide-react'
-import { RouteConfig } from "@/lib/config/routeConfig"
+"use client";
+import { RouteConfig } from "@/lib/config/routeConfig";
+import { Github } from "lucide-react";
+import Link from "next/link";
 
 export function Footer() {
-  return (
-    <footer className="border-t py-8 md:py-10">
-      <div className="container flex flex-col items-center justify-between gap-6 px-4 md:flex-row md:gap-8 md:px-6">
-        <div className="flex flex-col items-center gap-4 md:items-start md:gap-2">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            This is an <strong>unofficial</strong> wiki. All data is collected and maintained by the player community.
-          </p>
-        </div>
-        <div className="flex flex-col items-center gap-6 md:flex-row md:gap-8">
-          <nav className="flex flex-wrap justify-center gap-5 md:gap-6">
-            <Link href="mailto:toxicblade.work@gmail.com" className="text-sm font-medium hover:underline underline-offset-4">
-              Contact
-            </Link>
-            <Link href={RouteConfig.policy} className="text-sm font-medium hover:underline underline-offset-4">
-              Privacy
-            </Link>
-            <Link href={RouteConfig.terms} className="text-sm font-medium hover:underline underline-offset-4">
-              Terms
-            </Link>
-            <Link href={RouteConfig.contribute}
-            target="_blank"
-            className="text-sm font-medium hover:underline underline-offset-4">
-              Contribute
-            </Link>
-            <Link href={RouteConfig.discord}
-            target="_blank"
-            className="text-sm font-medium hover:underline underline-offset-4">
-              Discord
-            </Link>
-          </nav>
-          <div className="flex items-center gap-5 md:gap-6">
-            <Link
-              href={RouteConfig.contribute}
-              target="_blank"
-              rel="noreferrer"
-              className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-              <span className="sr-only">GitHub</span>
-              <Github className="h-5 w-5" />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </footer>
-  )
+	return (
+		<footer className="border-t py-8 md:py-10">
+			<div className="container flex flex-col items-center justify-between gap-6 px-4 md:flex-row md:gap-8 md:px-6">
+				<div className="flex flex-col items-center gap-4 md:items-start md:gap-2">
+					<p className="text-center text-muted-foreground text-sm leading-loose md:text-left">
+						This is an <strong>unofficial</strong> wiki. All data is collected
+						and maintained by the player community.
+					</p>
+				</div>
+				<div className="flex flex-col items-center gap-6 md:flex-row md:gap-8">
+					<nav className="flex flex-wrap justify-center gap-5 md:gap-6">
+						<Link
+							href="mailto:toxicblade.work@gmail.com"
+							className="font-medium text-sm underline-offset-4 hover:underline"
+						>
+							Contact
+						</Link>
+						<Link
+							href={RouteConfig.policy}
+							className="font-medium text-sm underline-offset-4 hover:underline"
+						>
+							Privacy
+						</Link>
+						<Link
+							href={RouteConfig.terms}
+							className="font-medium text-sm underline-offset-4 hover:underline"
+						>
+							Terms
+						</Link>
+						<Link
+							href={RouteConfig.contribute}
+							target="_blank"
+							className="font-medium text-sm underline-offset-4 hover:underline"
+						>
+							Contribute
+						</Link>
+						<Link
+							href={RouteConfig.discord}
+							target="_blank"
+							className="font-medium text-sm underline-offset-4 hover:underline"
+						>
+							Discord
+						</Link>
+					</nav>
+					<div className="flex items-center gap-5 md:gap-6">
+						<Link
+							href={RouteConfig.contribute}
+							target="_blank"
+							rel="noreferrer"
+							className="text-muted-foreground transition-colors hover:text-foreground"
+						>
+							<span className="sr-only">GitHub</span>
+							<Github className="h-5 w-5" />
+						</Link>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 }
--- lib/config/routeConfig.ts
+++ lib/config/routeConfig.ts
@@ -5,22 +5,21 @@ const BASE_URL = "/wiki";
 export const RouteConfig = {
 	home: `${BASE_URL}`,
 
-  discord: 'https://discord.gg/demonizedidle',
-  contribute: 'https://github.com/ToxicBlades/demonized-wiki',
+	discord: "https://discord.gg/demonizedidle",
+	contribute: "https://github.com/ToxicBlades/demonized-wiki",
 
 	guides: {
 		beginners_guide: `${BASE_URL}/beginners-guide`,
-		community_guides: `${BASE_URL}/community-guides`
+		community_guides: `${BASE_URL}/community-guides`,
 	},
 
-	content:{
+	content: {
 		items: `${BASE_URL}/items`,
-		character:`${BASE_URL}/character`,
-		locations:`${BASE_URL}/locations`,
-		resources:`${BASE_URL}/resources`,
+		character: `${BASE_URL}/character`,
+		locations: `${BASE_URL}/locations`,
+		resources: `${BASE_URL}/resources`,
 	},
 
 	terms: "/terms",
 	policy: "/policy",
-
 };
