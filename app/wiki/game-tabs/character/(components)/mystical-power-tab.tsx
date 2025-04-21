8579fdd2717ad00e6ad6694a5b592af474cd610b
Nick <128822732+ToxicBlades@users.noreply.github.com>
2025-04-21 04:36:55 +0300

    feat: initial character tab

---
+++ app/wiki/game-tabs/character/(components)/mystical-power-tab.tsx
@@ -0,0 +1,12 @@
+export default function MysticalPowerTab() {
+	return (
+		<div className="rounded-md p-4">
+			<h3 className="mb-2 font-bold text-xl">Mystical Powers</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
+				velit vel bibendum bibendum, nunc magna tincidunt nunc, nec aliquam nisl
+				nunc vel magna.
+			</p>
+		</div>
+	);
+}
---
+++ app/wiki/game-tabs/character/(components)/upgrade-tab.tsx
@@ -0,0 +1,12 @@
+export default function UpgradeTab() {
+	return (
+		<div className="rounded-md p-4">
+			<h3 className="mb-2 font-bold text-xl">Character Upgrades</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
+				velit vel bibendum bibendum, nunc magna tincidunt nunc, nec aliquam nisl
+				nunc vel magna.
+			</p>
+		</div>
+	);
+}
