45c9702ed2b9cce9ef9b5ceffd947de6239be4d8
Nick <128822732+ToxicBlades@users.noreply.github.com>
2025-04-21 03:09:37 +0300

    feat: mdx rendere + example mdx

---
+++ app/wiki/community-guides/example/page.tsx
@@ -0,0 +1,9 @@
+import Example from './example.mdx'
+
+export default function Page() {
+  return(
+    <div className='container py-5 px-5'>
+    <Example />
+    </div>
+  )
+}
---
+++ app/wiki/community-guides/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+	return (
+		<div className="container px-4 py-6">
+		 abra
+		</div>
+	);
+}
